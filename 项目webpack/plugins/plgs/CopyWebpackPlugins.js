6e53c366f79b49ecf8c8a556f5de0bbaa4f1a44b
zhaowei <781102725@qq.com>
2021-06-26 16:47:21 +0800

    add: copy plugin完成

--- 项目webpack/plugins/plgs/CopyWebpackPlugins.js
+++ 项目webpack/plugins/plgs/CopyWebpackPlugins.js
@@ -2,16 +2,21 @@
 /*
  * @Author: your name
  * @Date: 2021-06-25 01:02:38
- * @LastEditTime: 2021-06-25 01:41:37
+ * @LastEditTime: 2021-06-26 16:45:44
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /webpack/webpack-test/项目webpack/plugins/plgs/CopyWebpackPlugins.js
  */
 const { validate } = require('schema-utils');
 const path = require('path');
+const fs = require('fs');
+const { promisify } = require('util');
 const globby = require('globby');
+const webpack = require('webpack');
 const schema = require('./schema.json');
 
+const readFile = promisify(fs.readFile);
+const { RawSource } = webpack.sources;
 class CopyWebpackPlugins {
   constructor(options = {}) {
     // 验证options
@@ -23,26 +28,52 @@ class CopyWebpackPlugins {
 
   apply(compiler) {
     // 初始化compilation
-    compiler.hooks.thisCompilation.tap('CopyWebpackPlugin', async (compilation) => {
-      // 在添加资源添加钩子函数
-      compilation.hooks.additionalAssets.tapAsync(
-        'CopyWebpackPlugin',
-        async (cb) => {
-          // 将from中的资源复制到to
-          const { from, ignore } = this.options;
-          const to = this.options.to ? this.options.to : '.';
-          // 获取运行代码的根目录
-          const { context } = compiler.options;
-          const absoluteFrom = path.isAbsolute(from)
-            ? from
-            : path.resolve(context, from);
-          // globby(要处理的文件， options)
-          const paths = await globby(absoluteFrom, { ignore });
-          console.log(paths);
-          cb();
-        },
-      );
-    });
+    compiler.hooks.thisCompilation.tap(
+      'CopyWebpackPlugin',
+      async (compilation) => {
+        // 在添加资源添加钩子函数
+        compilation.hooks.additionalAssets.tapAsync(
+          'CopyWebpackPlugin',
+          async (cb) => {
+            // 将from中的资源复制到to
+            const { from, ignore } = this.options;
+            const to = this.options.to ? this.options.to : '.';
+            // 获取运行代码的根目录
+            const { context } = compiler.options;
+            const absoluteFrom = path.isAbsolute(from)
+              ? from
+              : path.resolve(context, from);
+            // globby(要处理的文件， options)
+            const paths = await globby(absoluteFrom, { ignore });
+            const files = await Promise.all(
+              paths.map(async (absPath) => {
+                const data = await readFile(absPath);
+                const fileName = path.join(to, absPath);
+                return {
+                  // 文件数据
+                  data,
+                  // 文件名称
+                  fileName,
+                };
+              }),
+            );
+            // 生成webpack格式的资源
+            const assets = files.map((file) => {
+              const source = new RawSource(file.data);
+              return {
+                source,
+                filename: file.fileName,
+              };
+            });
+            // 添加至compilation中
+            assets.forEach((asset) => {
+              compilation.emitAsset(asset.filename, asset.source);
+            });
+            cb();
+          },
+        );
+      },
+    );
   }
 }
 
--- 项目webpack/plugins/webpack.config.js
+++ 项目webpack/plugins/webpack.config.js
@@ -1,19 +1,21 @@
 /*
  * @Author: your name
  * @Date: 2021-06-25 01:00:41
- * @LastEditTime: 2021-06-25 01:41:51
+ * @LastEditTime: 2021-06-26 16:43:36
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /webpack/webpack-test/项目webpack/plugins/webpack.config.js
  */
 
+const path = require('path');
 const CopyWebpackPlugins = require('./plgs/CopyWebpackPlugins');
 
 module.exports = {
   plugins: [
     new CopyWebpackPlugins({
-      from: 'public/**/',
-      ignore: ['**/index.html'],
+      from: 'public/**',
+      // 也不知道为啥globby不能将文件目录给过滤掉输出，所以这里直接配置目录将其过滤
+      ignore: ['**/index.html', path.resolve(__dirname, 'public')],
     }),
   ],
 };
